73635c56dec04a6502ad89598c9c7c988b7e526d
Antoine BUREAU <abureau@e2r9p20.42.fr>
2016-03-10 17:08:18 +0100

    FastPush

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: abureau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/14 15:40:53 by abureau           #+#    #+#             */
-/*   Updated: 2016/03/08 17:36:06 by abureau          ###   ########.fr       */
+/*   Updated: 2016/03/10 17:08:06 by abureau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <sys/uio.h>
 #include <unistd.h>
 
-static int	ft_strlenn(const char *s)
+static size_t	ft_strlenn(const char *s)
 {
 	unsigned int	i;
 
@@ -26,60 +26,101 @@ static int	ft_strlenn(const char *s)
 	return (i);
 }
 
-static char	*ft_strredup(char *s1)
+static char	*ft_strrdup(char *s1)
 {
-	char	*dest;
+	char *dest;
 
-	dest = (char*)malloc(sizeof(char) * ft_strlenn(s1) + 2);
-	if (dest)
-	{
-		ft_strcpy(dest, &s1[ft_strlenn(s1) + 1]);
-		if (s1)
+	dest = malloc(sizeof(char) * (ft_strlen(s1) - ft_strlenn(s1) + 2));
+		if (dest)
 		{
+		ft_strcpy(dest, &s1[ft_strlenn(s1) + 1]);
+			if (s1)
+			{
 			free(s1);
 			s1 = NULL;
+			}
+			return (dest);
 		}
-		return (dest);
-	}
-	return (NULL);
+		return (NULL);
 }
+
+static int	parsing(char **tmpbuff, char **line, int const fd)
+{
+	size_t index;
+
+	index = 0;
+	*line = ft_strdup(ft_strsub(tmpbuff[fd], 0, ft_strlenn(tmpbuff[fd])));
+	tmpbuff[fd] = ft_strrdup(tmpbuff[fd]);
+	while (index <= 255)
+	{
+		if (tmpbuff[fd][index] == '\0')
+		{
+			return (1);
+		}
+		else if (tmpbuff[fd][index] == '\n')
+		{
+			return (1);
+		}
+		else
+			return (0);
+	}
+	return (0);
+}
+
+
+static void	lafonctionquimallocletableau(char ***buff)
+{
+	int index;
+	char **tmpbuff;
+
+	index = 0;
+	tmpbuff = NULL;
+	if (!*buff)
+	{
+		tmpbuff = (char**)malloc(sizeof(char*) * 256);
+			while(index < 256)
+			{
+				tmpbuff[index] = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1));
+				index++;
+			}
+	*buff = tmpbuff;
+	}
+}
+
 
 int			get_next_line(int const fd, char **line)
 {
 	char		buf[BUFF_SIZE + 1];
-	int			state;
 	int			ret;
-	static char	*tmpbuff = NULL;
+	static char	**tmpbuff;
 	int			out;
 
-	out = 0;
-	ret = 0;
-	state = 0;
+	lafonctionquimallocletableau(&tmpbuff);
+	ret = -42;
+	
 	if (fd < 0 || fd > 255)
 		return (-1);
-	if (!tmpbuff)
-		while ((ret = read(fd, buf, BUFF_SIZE)))
+		if (ft_strlenn(tmpbuff[fd]) == ft_strlen(tmpbuff[fd]))
 		{
+			ret = read(fd, buf, BUFF_SIZE);
 			if (ret < 0)
 				return (-1);
 			buf[ret] = '\0';
-			tmpbuff = ft_strjoin(tmpbuff, buf);
+			tmpbuff[fd] = ft_strjoin(tmpbuff[fd], buf);
+			out = 2;
 		}
-	else if (tmpbuff)
-	{
-		tmpbuff = ft_strredup(tmpbuff);
-		if (ret > 0 || ft_strlen(tmpbuff) >= 1)
-			if (state < ret || (state == 0 && ret == 0))
-			{
-				free(*line);
-				*line = ft_strdup(ft_strsub(tmpbuff, 0, ft_strlenn(tmpbuff)));
-			}
-		out = (ft_strlen(tmpbuff) == 0) ? 0 : 1;
-		if (out == 0)
+		else
 		{
-			free(tmpbuff);
-			tmpbuff = NULL;
+			out = parsing(tmpbuff, line, fd);
 		}
+	if (ret == 0)
+	{
+		parsing(tmpbuff, line, fd);
+		if (tmpbuff[fd])
+			tmpbuff[fd] = ft_strrdup(tmpbuff[fd]);
+		out = 0;
 	}
+	if (out == 2)
+		return (get_next_line(fd, line));
 	return (out);
 }
--- main.c
+++ main.c
@@ -6,26 +6,54 @@
 /*   By: abureau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/14 15:48:15 by abureau           #+#    #+#             */
-/*   Updated: 2016/03/08 15:46:50 by abureau          ###   ########.fr       */
+/*   Updated: 2016/03/10 15:56:57 by abureau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/includes/libft.h"
 #include "get_next_line.h"
 #include <fcntl.h>
-
+#include <stdio.h>
 int main(int a, char **b)
 {
-	int i = -1;
-	int j = 0;
-	char *line;
+	
+char *line;
 int fd;
-	//ft_putnbrnl(ft_atoi(b[2]));
-	if (a == 2)
+int fd2;
+int value;
+int j;
 		fd = open(b[1], O_RDONLY);
-	else
-		fd = 0;
-  
+		fd2 = open(b[2], O_RDONLY);
+	while (1)
+	{
+	if (a)
+		;
+//	int i = -1;
+//	int j = 0;
+	//ft_putnbrnl(ft_atoi(b[2]));
+	j = 0;
+		scanf("%d", &value);
+
+		if (value == 1)
+			j = get_next_line(fd, &line);
+		else if (value == 2)
+			j = get_next_line(fd2, &line);
+		ft_putstr("get_next_line result : ");ft_putendl(line);
+		ft_putstr("get_next_line return : ");ft_putnbrnl(j);
+	}
+	get_next_line(fd, &line);
+		ft_putstr("get_next_line result : ");ft_putendl(line);
+	get_next_line(fd2, &line);
+		ft_putstr("get_next_line resulti (fd2) : ");ft_putendl(line);
+	get_next_line(fd, &line);
+		ft_putstr("get_next_line result : ");ft_putendl(line);
+	get_next_line(fd2, &line);
+		ft_putstr("get_next_line result (fd2) : ");ft_putendl(line);
+	get_next_line(fd, &line);
+		ft_putstr("get_next_line result : ");ft_putendl(line);
+	get_next_line(fd2, &line);
+		ft_putstr("get_next_line resulti (fd2) : ");ft_putendl(line);
+ /* 
 	line = NULL;
 	ft_putendl("Starting For Loop");
 	for (i = 0; i < 13; i++)
@@ -42,7 +70,7 @@ int fd;
 		if (j == 1)
 			ft_strdel(&line);
 	}
-
+*/
 	/*
 	line = NULL;
 	if(a == 3)
