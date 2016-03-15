9ecc201768664a6d99f2fdf2924d67d1f4aeda5e
Antoine BUREAU <abureau@e1r10p7.42.fr>
2016-03-15 13:02:27 +0100

    FastPush

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,14 @@
 /*   By: abureau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/14 15:42:34 by abureau           #+#    #+#             */
-/*   Updated: 2016/03/10 17:54:13 by abureau          ###   ########.fr       */
+/*   Updated: 2016/03/14 15:08:13 by abureau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef		GET_NEXT_LINE_H
-# define	GET_NEXT_LINE_H
-
-
-# define BUFF_SIZE 5
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 1
 
 int		get_next_line(int const fd, char **line);
-int		ft_test(int q);
+
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abureau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/14 15:48:15 by abureau           #+#    #+#             */
-/*   Updated: 2016/03/10 18:11:47 by abureau          ###   ########.fr       */
+/*   Updated: 2016/03/15 13:02:19 by abureau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,25 @@
 #include <stdio.h>
 int main(int a, char **b)
 {
-	
-char *line;
-int fd;
-int fd2;
-if (a)
-	;
-fd = open(b[1], O_RDONLY);
-		fd2 = open(b[2], O_RDONLY);
+	char *line;
+	int fd = 0;
 	int i = -1;
 	int j = 0;
+
+	if (a)
+		i = 0;
+
+	fd = open(b[1], O_RDONLY);
 	j = 0;
 	line = NULL;
-	for (i = 0; i < 13; i++)
+while ((j = get_next_line(fd, &line)) >= 0)
 	{
-		j = get_next_line(fd, &line);
 		if (j == 1)
 			ft_putendl(line);
 		else if (j == 0)
 		{
 			ft_putendl("File ended");
+			break;
 		}
 		else if (j == -1)
 			ft_putendl("Error");
@@ -44,17 +43,7 @@ fd = open(b[1], O_RDONLY);
 		if (j == 1)
 			ft_strdel(&line);
 	}
-	/*
-	line = NULL;
-	if(a == 3)
-      	{
-		while(j  < ft_atoi(b[2]))
-		{
-			i = get_next_line(fd, &line);
-			ft_putstr("Line N_");ft_putnbr(j + 1);ft_putstr(" : ");ft_putendl(line);
-			i++;
-			j++;
-		}
-	}*/
+
+
 	return (0);
 }
